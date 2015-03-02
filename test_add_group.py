78a681055722b181312cedf0c735366c9b90bdb5
VLovets <vlovets@klika-tech.com>
2015-03-02 23:03:45 +0300

    Revert "Revert "Задание 2""
    This reverts commit 1347397d57fd8581c230156c1e3bef69ff02d4cb.

--- test_add_group.py
+++ test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
+from group import Group
 
 def is_alert_present(wd):
     try:
@@ -14,31 +14,59 @@ class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_(self):
-        success = True
-        wd = self.wd
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def log_in(self, wd, username, password):
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def opens_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def creation_group(self, wd, group):
+        # Init group creation
         wd.find_element_by_name("new").click()
+        # Fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new3")
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("new3")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("new3")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # Submit creation
         wd.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def log_out(self, wd):
         wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
+
+    def test_(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.log_in(wd, username="admin", password="secret")
+        self.opens_group_page(wd)
+        self.creation_group(wd, Group(name="new3", header="new3", footer="new3"))
+        self.return_to_groups_page(wd)
+        self.log_out(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.log_in(wd, username="admin", password="secret")
+        self.opens_group_page(wd)
+        self.creation_group(wd, Group(name="", header="", footer=""))
+        self.return_to_groups_page(wd)
+        self.log_out(wd)
 
     def tearDown(self):
         self.wd.quit()
