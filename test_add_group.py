9c006ab61c5440df7c48fadfbe35671673889a0b
VLovets <vlovets@klika-tech.com>
2015-03-02 21:35:55 +0300

    Провект PyCharm

--- test_add_group.py
+++ test_add_group.py
@@ -15,44 +15,31 @@ class test_add_group(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_test_add_group(self):
+    def test_(self):
         success = True
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secrest")
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
         wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
+        wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new1")
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select//option[1]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select//option[1]").click()
+        wd.find_element_by_name("group_name").send_keys("new3")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("new")
+        wd.find_element_by_name("group_header").send_keys("new3")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("new")
+        wd.find_element_by_name("group_footer").send_keys("new3")
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_link_text("groups").click()
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
         self.assertTrue(success)
-    
+
     def tearDown(self):
         self.wd.quit()
 
