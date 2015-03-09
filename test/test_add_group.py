d6e77f4d353080584c4d20d31583dff826b0812a
VLovets <vlovets@klika-tech.com>
2015-03-09 23:15:29 +0300

    Выделен класс для работы с группой

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,11 +12,11 @@ def app(request):
 
 def test_(app):
     app.session.log_in(username="admin", password="secret")
-    app.creation_group(GroupNew(name="new3", header="new3", footer="new3"))
+    app.group.creation(GroupNew(name="new3", header="new3", footer="new3"))
     app.session.log_out()
 
 
 def test_add_emp_group(app):
     app.session.log_in(username="admin", password="secret")
-    app.creation_group(GroupNew(name="", header="", footer=""))
+    app.group.creation(GroupNew(name="", header="", footer=""))
     app.session.log_out()
