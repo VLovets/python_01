cdd541831d8ae86c0fa289ef4ffb06c61e4196a6
VLovets <vlovets@klika-tech.com>
2015-03-15 20:05:50 +0300

    удаление групп если нет ни одной группы 2

--- fixture/group.py
+++ fixture/group.py
@@ -78,4 +78,4 @@ class GroupHelper:
     def count(self):
         wd = self.app.wd
         self.opens_group_page()
-        return len(wd.find_element_by_name("selected[]"))
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -1,6 +1,7 @@
 __author__ = 'vlovets'
 from model.group import GroupNew
 
+
 def test_delete_first_group(app):
     if app.group.count() == 0:
         app.group.creation(GroupNew(name="new"))
