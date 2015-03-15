81e5cc6549a5ecbad8ecc8e1ded4f67701eced9e
VLovets <vlovets@klika-tech.com>
2015-03-15 19:11:46 +0300

    Задание №8

--- fixture/session.py
+++ fixture/session.py
@@ -15,6 +15,28 @@ class SessionHelperAll:
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+
+    def ensure_log_in(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.log_out()
+        self.log_in(username, password)
+
     def log_out(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+
+    def ensure_log_out(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.log_out()
